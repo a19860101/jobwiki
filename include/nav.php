56a19d0c972d533a23c5c739864635fc2d8e2b44
Zac <a19860101@hotmail.com>
2018-09-16 22:59:26 +0800

    add nav

---
+++ include/nav.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-md navbar-light">
+    <div class="container">
+        <a href="#" class="navbar-brand">JOBWIKI</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navi"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navi">
+            <ul class="navbar nav ml-auto">
+                <li class="nav-item"><a href="#" class="nav-link">登入</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">登出</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">註冊</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+    require_once "config/database.php";
+    include "include/header.php";
+    include "include/nav.php";
+?>
+
+<?php include "include/footer.php"; ?>
